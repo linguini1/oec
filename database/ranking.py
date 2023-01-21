e870bf0011027d3baa05b2b937e6b49dad6530bb
linguini1 <matteo.golin@gmail.com>
2023-01-20 21:58:54 -0500

    Finished distance algorithm and added requirements file

--- database/ranking.py
+++ database/ranking.py
@@ -2,7 +2,7 @@
 __author__ = "Matteo Golin"
 
 # Imports
-from math import acos, sin, cos, degrees, asin, sqrt
+from math import sin, cos, radians, asin, sqrt
 from database.facility import Facility
 from database.patient import Patient
 
@@ -18,23 +18,17 @@ def distance_from_lat_lon_km(lat1: float, long1: float, lat2: float, long2: floa
     Solution from: https://www.omnicalculator.com/other/latitude-longitude-distance#:~:text=The%20distance%20between%20any%20two%20adjacent%20latitudes%20is%20approximately%2069,and%20meet%20at%20the%20poles.
     """
 
-    # Convert to degrees
-    lat1 = degrees(lat1)
-    lat2 = degrees(lat2)
-    long1 = degrees(long1)
-    long2 = degrees(long2)
+    # Convert to radians
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+    long1 = radians(long1)
+    long2 = radians(long2)
 
-    sin_squared_lat = (sin(lat2 - lat1) ** 2) / 2
-    sin_squared_long = (sin(long2 - long1) ** 2) / 2
+    sin_squared_lat = sin((lat2 - lat1) / 2) ** 2
+    sin_squared_long = sin((long2 - long1) / 2) ** 2
+    square_root = sqrt(sin_squared_lat + (cos(lat1) * cos(lat2) * sin_squared_long))
 
-    return 2 * EARTH_RADIUS_KM * asin(sqrt(sin_squared_lat)) + cos(lat1) * cos(lat2) * sin_squared_long
+    return 2 * EARTH_RADIUS_KM * asin(square_root)
 
 
-if __name__ == '__main__':
-    dist = distance_from_lat_lon_km(
-        43.747,
-        -79.744,
-        3.556,
-        -80.252
-    )
-    print(dist)
+
