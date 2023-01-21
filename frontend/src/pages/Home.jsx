decfe5701c47a51448e38e664710c7f17ff98b7c
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-01-21 00:42:54 -0500

    update home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,10 @@ import "./Home.css";
 
 export default function Home() {
   return (
-        
     <div id="home">
-    <h1 >
-    Need Help?
-    </h1>
+      
       <p>
-        Fill out your information on the form page to be matched with the
+        Fill out your information on the form page to be matched with the <br></br>
         nearest healthcare facility that can provide you treatment.
       </p>
     </div>
