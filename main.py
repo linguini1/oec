b0a314ed8734ca2eff21f38c7f282d63c07ca728
linguini1 <matteo.golin@gmail.com>
2023-01-20 20:36:27 -0500

    Added a bunch of routes

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ __author__ = "Matteo Golin"
 
 # Imports
 from database import load_facilities
+from flask import Flask
 
 # Constants
 
 
-# Main
-def main():
-    facils = load_facilities()
-    print(facils[0])
+# App creation
+app = Flask(__name__)
+
+
+# API routes
 
 
 if __name__ == '__main__':
-    main()
+    facilities = load_facilities()  # Load facilities before api is available
+    app.run(debug=True)  # TODO remove debug for submission
